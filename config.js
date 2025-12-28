3fca1d5fa6cffb4f914cd4721a59a15297223c8d
wykiesautomation <wykiesautomation@gmail.com>
2025-12-28 17:41:30 +0200

    Delete config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-
-<script>
-  window.CONFIG = {
-    // For now, call Apps Script directly (even if Cloudflare is up this works fine)
-    PAYMENTS_API_URL: 'https://script.google.com/macros/s/AKfycbwHuhjZ7YP7XMfJVrPlaBimM8dOweFtb6dCOe8QUOZlYAllepuSuJU7F52kzd20eMJZgQ/exec'
-    // Later, when you switch the Admin UI to use the proxy: 'https://admin-proxy.wykiesautomation.co.za/payments'
-  };
-</script>

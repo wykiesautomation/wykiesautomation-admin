8fcfe5fd6bda9392e06155357247bf37b350f4af
wykiesautomation <wykiesautomation@gmail.com>
2025-12-30 07:30:45 +0200

    Delete wykiesautomation-admin directory

--- wykiesautomation-admin/config.js
+++
@@ -1,6 +0,0 @@
-// Admin config
-window.CONFIG = {
-  // Replace with your real proxy endpoint if different
-  PAYMENTS_API_URL: 'https://admin-proxy.wykiesautomation.co.za/payments',
-  ALLOWLIST_EMAILS: ['wykiesautomation@gmail.com']
-};
